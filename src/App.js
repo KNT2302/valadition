6b40c3c096eb2aeb28002341f3496e07e104df64
Bbromw <kaystim6@gmail.com>
2022-08-18 16:15:12 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css'
-import FormInput from './component/form'
+import "./App.css"
+import FormInput from "./component/form"
+import Header from "./component/header/Header"
+import Home from "./component/homepage/Home"
+import { Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <FormInput />
+      <Header />
+      <hr />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/form">
+          <FormInput />
+        </Route>
+      </Switch>
     </div>
   )
 }
---
+++ src/component/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../navigation/Nav'
+import "./header.scss"
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>Header</div>
+      <Nav />
+    </header>
+  )
+}
+
+export default Header
